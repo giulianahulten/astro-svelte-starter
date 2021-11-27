18a64e930e4816c6f72d6136ba553ee53cb23fea
Giuliana Hulten <85498217+giulianahulten@users.noreply.github.com>
2021-11-26 20:43:23 -0700

    init

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  buildOptions: {
+    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true, // Generate sitemap (set to "false" to disable)
+  },
+  devOptions: {
+    // hostname: 'localhost',  // The hostname to run the dev server on.
+    // port: 3000,             // The port to run the dev server on.
+  },
+  renderers: ["@astrojs/renderer-svelte"],
+};
---
+++ src/components/Counter.svelte
@@ -0,0 +1,10 @@
+<script>
+  let count = 0;
+
+  const addToCount = () => {
+    count = count + 1;
+  };
+</script>
+
+<button on:click={addToCount}>clicka me</button>
+<p>{count}</p>
